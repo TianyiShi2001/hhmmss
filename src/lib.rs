409661659204d225acd8386eed1852e2ae3204dd
Michael Buesch <m@bues.ch>
2020-11-27 21:55:58 +0100

    Fix docstring of hhmmss()

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub trait Hhmmss {
     fn sms(&self) -> (i64, i64);
-    /// Pretty-prints a chrono::Duration in the form `HH:MM:SS.xxx`
+    /// Pretty-prints a chrono::Duration in the form `HH:MM:SS`
     fn hhmmss(&self) -> String {
         let (s, ms) = self.sms();
         s2hhmmss(s)
